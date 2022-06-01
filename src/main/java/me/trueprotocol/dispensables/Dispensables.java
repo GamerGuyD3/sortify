10bc5e2a0da2a6b9bc22d895517caefc4c75c502
TrueProtocol <49531692+GamerGuyD3@users.noreply.github.com>
2022-06-01 00:20:16 -0500

    Add files via upload

--- src/main/java/me/trueprotocol/dispensables/Dispensables.java
+++ src/main/java/me/trueprotocol/dispensables/Dispensables.java
@@ -8,26 +8,28 @@ import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
 import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import me.trueprotocol.dispensables.commands.*;
 import me.trueprotocol.dispensables.gui.ClickInventory;
+import me.trueprotocol.dispensables.gui.CloseInventory;
 import me.trueprotocol.dispensables.listeners.CancelDispense;
 import me.trueprotocol.dispensables.listeners.CancelDrop;
 import me.trueprotocol.dispensables.listeners.CancelHop;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
 
 public final class Dispensables extends JavaPlugin {
-
     private YamlDocument config;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         // bStats
-        int pluginId = 15323; // <-- Replace with the id of your plugin!
+        int pluginId = 15323; // <-- Id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
-        // Create and update config.yml
+        // BoostedYAML: Update config.yml
         try {
             config = YamlDocument.create(new File(getDataFolder(), "config.yml"), getResource("config.yml"),
                     GeneralSettings.DEFAULT, LoaderSettings.builder().setAutoUpdate(true).build(), DumperSettings.DEFAULT, UpdaterSettings.builder().setVersioning(new BasicVersioning("file-version")).build());
@@ -40,6 +42,7 @@ public final class Dispensables extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new CancelDrop(this), this);
         this.getServer().getPluginManager().registerEvents(new CancelHop(this), this);
         this.getServer().getPluginManager().registerEvents(new ClickInventory(this), this);
+        this.getServer().getPluginManager().registerEvents(new CloseInventory(this), this);
         this.getServer().getPluginCommand("dispensables").setExecutor(new DispensablesCommand(this));
         this.getServer().getPluginCommand("dispensables").setTabCompleter(new DispensablesCommand(this));
         this.getServer().getPluginCommand("dispenser").setExecutor(new DispenserCommand(this));
@@ -48,6 +51,7 @@ public final class Dispensables extends JavaPlugin {
         this.getServer().getPluginCommand("dropper").setTabCompleter(new DropperCommand(this));
         this.getServer().getPluginCommand("hopper").setExecutor(new HopperCommand(this));
         this.getServer().getPluginCommand("hopper").setTabCompleter(new HopperCommand(this));
+
     }
 
     @Override
